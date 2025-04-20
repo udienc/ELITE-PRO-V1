31e6cfaa01a7a9790f63237d5be8d9d75982b851
udienc <audiemilliantehn@gmail.com>
2025-04-20 09:00:09 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ global.ownernomer = "263782876561"
 global.ownername = "ElitePro"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
-global.location = "zimbabwe, Port Harcourt"
+global.location = "zimbabwe, Guruve"
 
 
 global.ownernumber = '263782876561'  //creator number
